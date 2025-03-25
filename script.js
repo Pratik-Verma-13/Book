bfe01855fd60b838ca03b8dc35619716cc0635f5
Pratik-Verma-13 <pratkverma13incredibles@gmail.com>
2025-03-25 23:30:21 +0530

    done

--- script.js
+++ script.js
@@ -1,129 +1,123 @@
-// let Books = {
-//     "book1": {
-//         "title": "The Alchemist",
-//         "author": "Paulo Coelho",
-//         "price": 10.00,
-//         "image": "images/alchemist.jpg"
-//     },
-//     "book2": {
-//         "title": "The Great Gatsby",
-//         "author": "F. Scott Fitzgerald",
-//         "price": 12.00,
-//         "image": "images/gatsby.jpg"
-//     },
-//     "book3": {
-//         "title": "The Catcher in the Rye",
-//         "author": "J.D. Salinger",
-//         "price": 15.00,
-//         "image": "images/catcher.jpg"
-//     },
-//     "book4": {
-//         "title": "To Kill a Mockingbird",
-//         "author": "Harper Lee",
-//         "price": 20.00,
-//         "image": "images/mockingbird.jpg"
-//     }
-// }
-
+// Search functionality
 let searchButton = document.getElementById('search-btn');
-
-
-searchButton.addEventListener('click', function(){
+searchButton.addEventListener('click', async function () {
+    window.location.href = "#searched-books";
     let searchBox = document.getElementById('input-box');
     let searchValue = searchBox.value.toLowerCase();
     searchBox.value = "";
     let displaySearchedBooks = document.getElementById('searched-book-list');
     displaySearchedBooks.innerHTML = "";
     let showSearch = document.getElementById('searched-books');
-    if(searchValue == ""){
+    if (searchValue == "") {
         alert("Please enter a search value");
         return
     }
     showSearch.style.display = "none";
-    getBooks().then(books => {
-        for (let key in books) {
-            let book = books[key];
-            let title = book.title.toLowerCase();
-            let author = book.author.toLowerCase();
-            if (title.includes(searchValue) || author.includes(searchValue)) {
-                console.log("hello");
-                let child = document.createElement('div');
-                child.innerHTML = `
-                <div id="searched-book-card">
-                    <div id="search-book-title">Book Title : ${book.title}</div>
-                    <div id="search-book-author">Book Author : ${book.author}</div>
-                    <div id="search-book-price">Book Price : &#8377; ${book.price}</div>
-                </div>
-                `;
-                displaySearchedBooks.appendChild(child);
-            }
+    let books = await getBooks();
+    console.log(books);
+    for (let key in books) {
+        let book = books[key];
+        let title = book.title.toLowerCase();
+        let author = book.author.toLowerCase();
+        if (title.includes(searchValue) || author.includes(searchValue)) {
+            console.log("hello");
+            let child = document.createElement('div');
+            child.innerHTML = `
+            <div id="searched-book-card">
+                <div id="search-book-title">Book Title : ${book.title}</div>
+                <div id="search-book-author">Book Author : ${book.author}</div>
+                <div id="search-book-price">Book Price : &#8377; ${book.price}</div>
+            </div>
+            `;
+            displaySearchedBooks.appendChild(child);
+
         }
-    });
-    if (displaySearchedBooks.innerHTML != "") {
-        showSearch.style.display = "block";
         
     }
-    else{
+    if (displaySearchedBooks.innerHTML == "") {
         alert("Sorry, No books found");
-    }   
+        return
+    }
+    else {
+        showSearch.style.display = "block";
+    }
+    
 });
 
-
+// Retreving books from json file
 async function getBooks() {
     let response = await fetch('books.json');
-    let data = await response.json();  
+    let data = await response.json();
     return data;
 }
 
-function clearInput(){
+
+// Clearing input box
+function clearInput() {
     let searchBox = document.getElementById('input-box');
     searchBox.value = "";
 }
 
 // Displaying books according to price
-
-function sortBooks(){
+function sortBooks() {
     getBooks().then(books => {
         books.sort((a, b) => a.price - b.price);
         // console.log(books);
     });
 }
 
-sortBooks();
+// Storing books in local storage from json file
 window.addEventListener('load', () => {
     getBooks().then(books => {
-        for(let key in books){
+        for (let key in books) {
             let book = books[key];
             localStorage.setItem(`${book.id}`, `${JSON.stringify(book)}`);
         }
     })
 })
 
-
-function addBooks(){
+// adding new books
+let submitAdd = document.getElementById('submit-add-form');
+submitAdd.addEventListener('click', addBooks);
+function addBooks() {
     let addTitle = document.getElementById('add-book-name');
     let addAuthor = document.getElementById('add-author-name');
     let addPrice = document.getElementById('add-price');
-    let addId = document.getElementById('add-id');  
-    if(addTitle.value == "" || addAuthor.value == "" || addPrice.value == "" || addId.value == ""){
+    let addId = document.getElementById('add-id');
+    if (addTitle.value == "" || addAuthor.value == "" || addPrice.value == "" || addId.value == "") {
         alert("Please fill the fields");
         return
     }
     let book = {
-        "id": addId.value,
         "title": addTitle.value,
         "author": addAuthor.value,
-        "price": addPrice.value
+        "price": addPrice.value,
+        "id": addId.value,
     }
-    localStorage.setItem(`${book.id}`, `${JSON.stringify(book)}`);   
+    localStorage.setItem(`${book.id}`, `${JSON.stringify(book)}`);
     alert("Book added successfully");
 }
 
-function removeBooks(){
+// removing books
+function removeBooks() {
     let removeId = document.getElementById('remove-id');
     localStorage.removeItem(`${removeId.value}`);
     alert("Book removed successfully");
 }
 
-let submitAdd = document.getElementById('submit-add-form');
-submitAdd.addEventListener('click', addBooks);
+// Adding books to cart
+addToCart = Array.from(document.getElementsByClassName('add-to-cart'));
+addToCart.forEach(element => {
+    element.addEventListener('click', () => {
+        cartArray = localStorage.getItem('cart')
+        if (cartArray == null) {
+            cartArray = [];
+            localStorage.setItem("cart", JSON.stringify(cartArray));
+        }
+        else {
+            cartArray = JSON.parse(cartArray);
+            cartArray.push(element.parentElement.children[0].innerText);
+            localStorage.setItem("cart", JSON.stringify(cartArray));
+        }
+    })
+})
